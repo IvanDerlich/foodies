0a9262095b4e2d99579b24c115867f0c5a6e858a
Ivan Derlich <a@ivanderlich.com>
2024-03-13 03:05:50 -0300

    Fix build

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
+        Time to get started!!
         <p>
           <Link href="/meals">Meals</Link>
         </p>
