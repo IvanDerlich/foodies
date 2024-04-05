6e4c2d0ab91eb2080acf188e20eb27d70c04ff9a
Ivan Derlich <a@ivanderlich.com>
2024-04-05 18:52:23 -0300

    Add 'meal not found' page

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import { getMeal } from '@/db/actions'
+import { notFound } from 'next/navigation'
 import classes from './page.module.css'
 import type { Meal } from '../types/meal'
 
 async function MealDetals({ params: { mealSlug } }) {
   const meal: Meal = await getMeal(mealSlug)
+  if (!meal) {
+    notFound()
+  }
   meal.instructions = meal.instructions.replace(/\n/g, '<br>')
   return (
     <>
