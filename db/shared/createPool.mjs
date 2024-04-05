95067041705f07201d12748f5a8a8beafdff52a9
Ivan Derlich <a@ivanderlich.com>
2024-04-05 17:46:50 -0300

    Remove console log

--- db/shared/createPool.mjs
+++ db/shared/createPool.mjs
@@ -4,7 +4,6 @@ const { Pool } = pg
 
 const createPool = (database) => {
   const connectionString = `${process.env.CONNECTION_STRING_BEGINING}/${database}?${process.env.CONNECTION_STRING_ENDING}`
-  console.log('Connection String: ', connectionString)
   return new Pool({
     connectionString,
   })
