a0b7f9e77543ec8429d11f65a8433448ff9ef00e
Ivan Derlich <a@ivanderlich.com>
2024-04-05 17:24:49 -0300

    Console log connection string

--- db/shared/createPool.mjs
+++ db/shared/createPool.mjs
@@ -3,8 +3,10 @@ import pg from 'pg'
 const { Pool } = pg
 
 const createPool = (database) => {
+  const connectionString = `${process.env.CONNECTION_STRING_BEGINING}/${database}?${process.env.CONNECTION_STRING_ENDING}`
+  console.log('Connection String: ', connectionString)
   return new Pool({
-    connectionString: `${process.env.CONNECTION_STRING_BEGINING}/${database}?${process.env.CONNECTION_STRING_ENDING}`,
+    connectionString,
   })
 }
 
