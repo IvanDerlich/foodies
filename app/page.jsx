f1a24473dd77423a4d3150678f9fd58c47c2d4d9
Ivan Derlich <a@ivanderlich.com>
2024-05-21 14:28:46 -0300

    Small change to test pipeline and deploy

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
-            <p>Taste & share food from all over the world.</p>
+            <p>Taste and share food from all over the world.</p>
           </div>
           <div className={classes.cta}>
             <Link href="/community">Join the Community</Link>
